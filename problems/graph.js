383b1d6c705679bb675820b8cfb821f24ae88bec
Laura <lkzailac@gmail.com>
2021-03-03 15:37:45 -0500

    middle of breadth first

--- problems/graph.js
+++ problems/graph.js
@@ -25,12 +25,46 @@ class Graph {
     this.adjList[destValue].push(srcValue);
   }
 
+  //receiving an 2D array of edges
+  //cycle through every edge
+  //create an edge within new graph for every edge
+  //creating an adjList with the edges provided
+  //edges.forEach >>
+    //if we dont have key, create new key
+    //if we do, we add the second element of edge array as a neighbor to the key
+
   buildGraph(edges) {
-    // Code goes here ...
+    edges.forEach(edge => {
+      this.addEdges(edge[0], edge[1]);
+    })
+    return this.adjList;
   }
+  //visit vertex
+  //then visit ALL children
+  //keep track of visited as a Set
+  //should examine key vertex
 
   breadthFirstTraversal(startingVertex) {
-    // Code goes here ...
+    const visitedQ = [];
+    const allV = [];
+    visited.push(startingVertex);
+
+
+    for(let vertex in this.adjList) {
+      //cycle through neighbors
+      //if they havent been visited add them to visited, other wise skip(continue)
+      //console.log(typeof vertex);
+
+      //visit neighbors
+      this.adjList[vertex].forEach(neighbor => {
+        if(!visited.includes(neighbor)) {
+          visited.push(neighbor);
+        }
+        //if it has been visited..
+      })
+    }
+    console.log(Array.from(visited));
+    return Array.from(visited);
   }
 
   depthFirstTraversalIterative(startingVertex) {
@@ -46,12 +80,3 @@ class Graph {
 module.exports = {
   Graph
 };
-
-
-
-
-
-
-
-
-
